d37fb9fc89f99b14570735024194f3aa321636af
ParadoxRival <normatovjasur048@gmail.com>
2025-01-28 13:02:46 +0300

    settings fix

--- admin_panel/settings.py
+++ admin_panel/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'admin_panel.accounts',
     'rest_framework',
     'django_extensions'
 ]
