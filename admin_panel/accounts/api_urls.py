8d7df9964406eacbbc8c154b6f11b82d81f48d0e
ParadoxRival <normatovjasur048@gmail.com>
2025-01-28 15:48:01 +0300

    set secret key

--- admin_panel/accounts/api_urls.py
+++ admin_panel/accounts/api_urls.py
@@ -1,6 +1,7 @@
 from .views import content_list_api, increment_views_api
 from django.urls import path
 from .views import compare_image_api
+
 print("🔧 api_urls.py загружен!")
 urlpatterns = [
     path('content/', content_list_api, name='content_list_api'),
